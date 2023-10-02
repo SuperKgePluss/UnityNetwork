624b40fba2c0ae95ced68a7e1330f7a5301ec7eb
SuperKgePluss <ma.puttichawat@outlook.com>
2023-10-02 22:07:21 +0700

    Network variable
    Network variable with custom data type.

--- Assets/Scripts/PlayerNetwork.cs
+++ Assets/Scripts/PlayerNetwork.cs
@@ -1,12 +1,31 @@
+using Unity.Collections;
 using Unity.Netcode;
 using UnityEngine;
 
 public class PlayerNetwork : NetworkBehaviour {
-    NetworkVariable<int> randomNumber = new NetworkVariable<int>(1, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+    NetworkVariable<MyCustomData> randomNumber = new NetworkVariable<MyCustomData>(
+        new MyCustomData {
+            _int = 56,
+            _bool = true,
+        },
+        NetworkVariableReadPermission.Everyone,
+        NetworkVariableWritePermission.Owner);
+
+
+    public struct MyCustomData : INetworkSerializable {
+        public int _int;
+        public bool _bool;
+        public FixedString128Bytes message;
+
+        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter {
+            serializer.SerializeValue(ref _int);
+            serializer.SerializeValue(ref _bool);
+        }
+    }
 
     public override void OnNetworkSpawn() {
-        randomNumber.OnValueChanged += (int previousValue, int newValue) => {
-            Debug.Log(OwnerClientId + ", RandomNumber: " + randomNumber.Value);
+        randomNumber.OnValueChanged += (MyCustomData previousValue, MyCustomData newValue) => {
+            Debug.Log(OwnerClientId + ", " + newValue._int + ", " + newValue._bool + "; " + newValue.message);
         };
     }
 
@@ -14,7 +33,11 @@ public class PlayerNetwork : NetworkBehaviour {
         if (!IsOwner) return;
 
         if (Input.GetKeyDown(KeyCode.T)) {
-            randomNumber.Value = Random.Range(0, 100);
+            randomNumber.Value = new MyCustomData {
+                _int = 10,
+                _bool = false,
+                message = "APPPPPP",
+            };
         }
 
         Vector3 moveDir = new Vector3(0, 0, 0);
